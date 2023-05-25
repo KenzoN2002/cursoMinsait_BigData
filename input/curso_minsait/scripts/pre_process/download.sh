827f068b8605a2927653c6c1e1ac743f9dc4af94
Kenzo Lima <owamlima92@gmail.com>
2023-05-25 22:26:29 +0000

    Exercicio de baixar arquivos completo

--- input/curso_minsait/scripts/pre_process/download.sh
+++ input/curso_minsait/scripts/pre_process/download.sh
@@ -3,5 +3,7 @@
 cd ../../raw
 mkdir categoria
 cd categoria
-for i in categoria 
-curl -O  https://raw.githubusercontent.com/caiuafranca/dados_curso/main/categoria.csv
+meuArray=("alunos" "cidade" "cliente" "estado" "filial" "item_pedido" "parceiro" "produto" "subcategoria")
+for str in ${meuArray[@]}; do
+    curl -O  https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$str.csv
+done
